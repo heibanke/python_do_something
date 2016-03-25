1aca5b757448d0594ed237ecc7c48ba64de6fa90
heibanke <heibanke@aliyun.com>
2016-03-25 10:33:51 +0800

    Update crawler_requests.py
    modify https

--- Code/Chapter6/spider/crawler_requests.py
+++ Code/Chapter6/spider/crawler_requests.py
@@ -5,14 +5,14 @@ r.json()
 r.encoding
 r.text
 
-r = requests.get('http://github.com')
+r = requests.get('https://www.github.com')
 r.url
 r.history
 
 
-r = requests.get('http://github.com', allow_redirects=False)
+r = requests.get('https://www.github.com', allow_redirects=False)
 
-requests.get('http://github.com', timeout=0.001)
+requests.get('https://www.github.com', timeout=0.001)
 
 
 url = "http://www.heibanke.com/lesson/crawler_ex01/"
